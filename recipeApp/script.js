b3fd51054263ef762be947a0b641e87e5f44d173
eddev <edwardbrian96@gmail.com>
2020-09-14 23:06:25 -0500

    function save in localstorage

--- recipeApp/script.js
+++ recipeApp/script.js
@@ -1,14 +1,16 @@
 const mealsList = document.getElementById('meals')
+const favMealList = document.getElementById('fav_meals')
 const search = document.getElementById('search_term')
 const btnSearch = document.getElementById('search')
 
+
 getRandonMeal()
+fetchFavMeals()
 
 btnSearch.addEventListener('click', async () => {
-
     // cleaner container
     mealsList.innerHTML = ""
-    const allMeals = await queryApi(search.value)
+    const allMeals = await getMealBySearch(search.value)
 
     if (allMeals) {
         allMeals.forEach(element => {
@@ -26,7 +28,16 @@ async function getRandonMeal() {
     createMeal(randomMeal)
 }
 
-async function queryApi(model) {
+async function fetchFavMeals(){
+    const mealIds = getMealIds()
+    for (let index = 0; index < mealIds.length; index++) {
+        const element = mealIds[index];
+        meal = await fecthMealId(element)
+        createMealFav(meal)
+    }
+}
+
+async function getMealBySearch(model) {
     // search meal of the user from input
     const responde = await fetch(
         `https://www.themealdb.com/api/json/v1/1/search.php?s=${model}`
@@ -36,10 +47,21 @@ async function queryApi(model) {
     return meals
 }
 
+async function fecthMealId(idMeal){
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal
+    );
+    const respData = await resp.json()
+    const meal = respData.meals[0]
+    return meal
+}
+
+
 function createMeal(model) {
     // create the design and show the information , create btn add meals
     // favouritte
     const meal = document.createElement('div')
+    meal.classList.add("meal");
     meal.innerHTML = `
         <div class="meal_header">
             <img src="${model.strMealThumb}"/>
@@ -62,25 +84,28 @@ function createMeal(model) {
             saveMealsLocalS(model.idMeal);
         }
     })
-
     mealsList.appendChild(meal)
+}
+
+function createMealFav(model){
+    // show the favourite meal 
+    const favMeal = document.createElement('li')
+    favMeal.innerHTML=`
+        <img src="${model.strMealThumb}"/>
+    `;
+    favMealList.appendChild(favMeal)
 }
 
 function saveMealsLocalS(model) {
     // save data in the localstorage
     const mealIds = getMealIds()
-
     localStorage.setItem('mealIds', JSON.stringify([
         ...mealIds, model
     ]))
-
-    console.log(JSON.parse(localStorage.getItem('mealIds')));
 }
 
 function getMealIds() {
-    // get key from meal if exits that
+    // get key from all meals if exits in the local storage
     const mealIds = JSON.parse(localStorage.getItem('mealIds'))
     return mealIds === null ? [] : mealIds 
 }
-
-
